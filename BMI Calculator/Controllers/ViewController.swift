c4cd653c738642aad4aec629d02194fcf63055e2
Haley-1617 <alicelai2920@gmail.com>
2021-05-18 05:14:57 -0700

    Built entire UI view from code

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0fkg", sender.value)
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let bmi = weightSlider.value / pow(heightSlider.value, 2)
+        print(bmi)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+    }
 }
 
